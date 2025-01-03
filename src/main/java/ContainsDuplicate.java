cb6cd477e2ab44a0966a72d2448325d16d93e2f6
pitki <schakravarty.59@gmail.com>
2025-01-03 21:05:15 +0100

    chore: wip contains duplicate

---
+++ src/main/java/ContainsDuplicate.java
@@ -0,0 +1,11 @@
+public class ContainsDuplicate {
+    /**
+     * Given an integer array nums,
+     * return true if any value appears at least
+     * twice in the array, and return false if every element is distinct.
+     **/
+// https://leetcode.com/problems/contains-duplicate/description/
+    public static void main(String[] args) {
+        System.out.println("contains duplicate");
+    }
+}
